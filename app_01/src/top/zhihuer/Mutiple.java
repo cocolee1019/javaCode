ac7fd63c08a851ff6f74f0617caa28972a6376e4
CocoLee <ljj@zhihuer.top>
2017-04-20 22:05:49 +0800

    second modfiy

--- app_01/src/top/zhihuer/Mutiple.java
+++ app_01/src/top/zhihuer/Mutiple.java
@@ -2,7 +2,15 @@ package top.zhihuer;
 
 public class Mutiple {
 
-	public static void main(String[] args){
-		System.out.println(10*100);
+	public static void main(String[] args) {
+		System.out.println(mutiple(19, 20));
+	}
+
+	static int mutiple(int x, int y) {
+		return x * y;
+	}
+
+	static int addation(int x, int y) {
+		return x + y;
 	}
 }
---
+++ app_01/src/top/zhihuer/indavial.java
@@ -0,0 +1,8 @@
+package top.zhihuer;
+
+public class indavial {
+
+	public static float indavi(int x,int y){
+		return x/y;
+	}
+}
